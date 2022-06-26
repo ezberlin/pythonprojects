d90a5bdb3c6030e4b6679f917d5060eb0b633a8e
ezberlin <60936497+ezberlin@users.noreply.github.com>
2022-06-26 11:05:17 +0200

    manipulate lists with for loops

--- ch3/dinner.py
+++ ch3/dinner.py
@@ -3,6 +3,7 @@ guests = ["Donald Duck", "Micky Mouse", "Daisy Duck"]
 print(f"Dear {guests[0]}, you are invited to a dinner at me today at 7 o'clock.")
 print(f"Dear {guests[1]}, you are invited to a dinner at me today at 7 o'clock.")
 print(f"Dear {guests[2]}, you are invited to a dinner at me today at 7 o'clock.")
+print(len(guests))
 #3-5
 print()
 print(f"{guests[2]} can't come. ")
@@ -12,6 +13,7 @@ print()
 print(f"Dear {guests[0]}, you are invited to a dinner at me today at 7 o'clock.")
 print(f"Dear {guests[1]}, you are invited to a dinner at me today at 7 o'clock.")
 print(f"Dear {guests[2]}, you are invited to a dinner at me today at 7 o'clock.")
+print(len(guests))
 #3-6
 print()
 print("We've got a bigger table")
@@ -26,6 +28,7 @@ print(f"Dear {guests[2]}, you are invited to a dinner at me today at 7 o'clock."
 print(f"Dear {guests[3]}, you are invited to a dinner at me today at 7 o'clock.")
 print(f"Dear {guests[4]}, you are invited to a dinner at me today at 7 o'clock.")
 print(f"Dear {guests[5]}, you are invited to a dinner at me today at 7 o'clock.")
+print(len(guests))
 #3-7
 print()
 print("Only two people can be invited")
@@ -43,4 +46,4 @@ print(f"Dear {popped_person_3}, ")
 print("I'm sorry to tell you that you can't be invited, because the new table can't be delivered early enough.")
 print(f"Dear {popped_person_4}, ")
 print("I'm sorry to tell you that you can't be invited, because the new table can't be delivered early enough.")
-
+print(len(guests))
--- ch3/worldtrip.py
+++ ch3/worldtrip.py
@@ -1,9 +1,24 @@
 cities = ["Hamburg", "Kairo", "Paris", "Kopenhagen", "Amsterdam"]
 print(cities)
+
+print("")
 print(sorted(cities))
 print(cities)
+
+print("")
 print(sorted(cities,reverse=True))
+print(cities)
+
+print("")
+cities.reverse()
 print(cities)
 cities.reverse()
 print(cities)
 
+print("")
+cities.sort()
+print(cities)
+
+print("")
+cities.sort(reverse=True)
+print(cities)
