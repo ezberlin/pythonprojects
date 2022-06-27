f0c5aa8abfe07f2d284b4daae7f9fe7d613726a5
ezberlin <60936497+ezberlin@users.noreply.github.com>
2022-06-27 22:55:31 +0200

    function lists and range()

---
+++ ch4/animals.py
@@ -0,0 +1,5 @@
+animals = ["dolphin", "clownfish", "frog"]
+for animal in animals:
+    print(f"A {animal} lives in the water.")
+
+print("\nAny of these animals is living in the water!")
---
+++ ch4/pizzas.py
@@ -0,0 +1,5 @@
+pizzas = ["mozzarella", "peperoni", "banana"]
+for pizza in pizzas:
+    print(f"I love {pizza.title()} pizza!")
+    
+print("\nI love every pizza!")
