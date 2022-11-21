691bfffd3729da2db1c5c617052f1299f94fcb01
ezberlin <60936497+ezberlin@users.noreply.github.com>
2022-11-21 22:40:02 +0100

    added case n = 1

--- nochapter/mersenne_prime_project/prime_detector.py
+++ nochapter/mersenne_prime_project/prime_detector.py
@@ -4,6 +4,8 @@ def isNumberPrime(n):
     dividers = 0 #dividers without 1
     if n.isdigit():
         n = int(n)
+        if n == 1:
+            return 0
         for i in range(1, int(math.sqrt(n) + 1)):
             if(n / i == int(n / i)):
                 dividers = dividers + 1
@@ -11,10 +13,9 @@ def isNumberPrime(n):
                     break
         if(dividers == 1):
             return 1
-        else:
-            return 0
-            print("too low divider number")
     else:
         return 0
         print("no integer as n")
+        
+        
     
