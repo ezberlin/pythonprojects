048c40260082c5805d9ec348874dc2b6326bc34c
ezberlin <60936497+ezberlin@users.noreply.github.com>
2022-11-24 20:21:14 +0100

    developed much more efficent way

---
+++ nochapter/mersenne_prime_project/mersenne_prime_detector2.py
@@ -0,0 +1,32 @@
+def isNumberMersenne(n):
+    x = n + 1
+    while x != 1:
+        x = 0.5*x
+        if x != int(x):
+            break
+    if x == 1:
+        return 1
+    else:
+        return 0
+    
+def isNumberPrime(n):
+    n = str(n)
+    dividers = 0
+    n = int(n)
+    if n == 1:
+        return 0
+    for i in range(1, int(n ** 0.5) + 1):
+        if(n / i == int(n / i)):
+            dividers = dividers + 1
+            if dividers == 2:
+                break
+    if(dividers == 1):
+        return 1
+    else:
+        return 0
+
+mp_number = 0
+for n in range(1,1024):
+    if isNumberPrime(2**n-1):
+        mp_number = mp_number + 1
+        print(f"{mp_number}: {2**n-1} (2^{n}-1)")
