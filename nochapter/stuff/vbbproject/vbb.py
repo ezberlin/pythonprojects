d6499798ee634efc5d76b82c4009f8a7b771951e
ezberlin <60936497+ezberlin@users.noreply.github.com>
2024-01-14 12:13:41 +0100

    vbb

--- nochapter/stuff/vbbproject/vbb.py
+++ nochapter/stuff/vbbproject/vbb.py
@@ -1,1 +1,27 @@
 import requests
+import json
+def cutLocation(location):
+    percent = False
+    newLocation = ""
+    for character in location:
+        if percent:
+            newLocation += character
+        if character == "%":
+            percent = True
+    return newLocation.strip()
+
+
+latitude = input("Latitude ")
+longitude = input("Longitude ")
+
+api_url = "https://vbb.demo.hafas.de/fahrinfo/restproxy/2.32/location.nearbystops"
+params = {"format": "json", "accessId":"accesId", "originCoordLat":latitude, "originCoordLong":longitude, "maxNo":"1"}
+response = requests.get(api_url, params=params)
+response.encoding = 'utf-8'
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(response.json(), f, ensure_ascii=False, indent=4)
+try:
+    location = cutLocation(response.json()["stopLocationOrCoordLocation"][0]["StopLocation"]["LocationNotes"]["LocationNote"][-1]["value"])
+    print(location)
+except KeyError:
+    print("Kein Bahnhof im 1km Radius lol")
