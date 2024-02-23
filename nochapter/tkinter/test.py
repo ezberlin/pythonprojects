4cd3014e4e186b9d9f59dda070ccdb0c65650fda
ezberlin <60936497+ezberlin@users.noreply.github.com>
2024-02-23 19:24:24 +0100

    resize window

--- nochapter/tkinter/test.py
+++ nochapter/tkinter/test.py
@@ -1,8 +1,21 @@
 import tkinter as tk
 
+#create window
 root = tk.Tk()
+root.title("Rickroll")
 
+#resize window
+root.geometry("1080x720")
+root.minsize(720, 480)
+root.maxsize(1920, 1080)
+root.resizable(width=True, height=True)
+
+#text
 label1 = tk.Label(root, text="Never gonna give you up")
 label1.pack()
 
+#main loop
 root.mainloop()
+
+#exit
+print("Fenster geschlossen")
